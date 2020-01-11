900f41c815b61980a0e6419dcfb10465cb426f53
kawatech <kawatech@gol.com>
2020-01-11 22:24:41 +0900

    2回目、Snackberのviewがエラーになる

--- app/src/main/java/jp/techacademy/naoki/kawamata/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/naoki/kawamata/calcapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import android.support.design.widget.Snackbar
 
 class MainActivity : AppCompatActivity() , View.OnClickListener {
 
@@ -22,12 +23,40 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
         when(v.id){
             R.id.button1 -> {
                 val intent = Intent(this, SecondActivity::class.java)
-                intent.putExtra("VALUE1", editText1.text.toString())
+                val v1 = editText1.text.toString().toFloat()
+                val v2 = editText2.text.toString().toFloat()
+                if (v1 == null) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+                }
+                val v3 = v1 + v2
+                intent.putExtra("VALUE1", v3)
                 startActivity(intent)
             }
-         //   R.id.button2 -> showAlertDialog()
-         //   R.id.button3 -> showTimePickerDialog()
-         //   R.id.button4 -> showDatePickerDialog()
+            R.id.button2 -> {
+                val intent = Intent(this, SecondActivity::class.java)
+                val v1 = editText1.text.toString().toFloat()
+                val v2 = editText2.text.toString().toFloat()
+                val v3 = v1 - v2
+                intent.putExtra("VALUE1", v3)
+                startActivity(intent)
+            }
+            R.id.button3 -> {
+                val intent = Intent(this, SecondActivity::class.java)
+                val v1 = editText1.text.toString().toFloat()
+                val v2 = editText2.text.toString().toFloat()
+                val v3 = v1 * v2
+                intent.putExtra("VALUE1", v3)
+                startActivity(intent)
+            }
+            R.id.button4 -> {
+                val intent = Intent(this, SecondActivity::class.java)
+                val v1 = editText1.text.toString().toFloat()
+                val v2 = editText2.text.toString().toFloat()
+                val v3 = v1 / v2
+                intent.putExtra("VALUE1", v3)
+                startActivity(intent)
+            }
         }
     }
 }
