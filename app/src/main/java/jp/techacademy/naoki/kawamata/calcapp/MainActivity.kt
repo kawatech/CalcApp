9c447fb8a43b5704e6499b5a361c0a37e4718574
kawatech <kawatech@gol.com>
2020-01-12 15:01:33 +0900

    3回目、提出の1回目

--- app/src/main/java/jp/techacademy/naoki/kawamata/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/naoki/kawamata/calcapp/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import android.support.design.widget.Snackbar
+
 
 class MainActivity : AppCompatActivity() , View.OnClickListener {
 
@@ -23,40 +23,78 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
         when(v.id){
             R.id.button1 -> {
                 val intent = Intent(this, SecondActivity::class.java)
-                val v1 = editText1.text.toString().toFloat()
-                val v2 = editText2.text.toString().toFloat()
-                if (v1 == null) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show()
+                val x1 = editText1.text.toString().length
+                val x2 = editText2.text.toString().length
+
+                if ((x1 >= 1) && (x2 >= 1)){
+                    var v1 = editText1.text.toString().toFloat()
+                    var v2 = editText2.text.toString().toFloat()
+                    var v3 = v1 + v2
+                    intent.putExtra("VALUE1", v3)
+                    startActivity(intent)
                 }
-                val v3 = v1 + v2
-                intent.putExtra("VALUE1", v3)
-                startActivity(intent)
+                else {
+                    textView.text = "数値が入力されていません"
+
+                }
+
+
+
+
+
             }
             R.id.button2 -> {
                 val intent = Intent(this, SecondActivity::class.java)
-                val v1 = editText1.text.toString().toFloat()
-                val v2 = editText2.text.toString().toFloat()
-                val v3 = v1 - v2
-                intent.putExtra("VALUE1", v3)
-                startActivity(intent)
+                val x1 = editText1.text.toString().length
+                val x2 = editText2.text.toString().length
+
+                if ((x1 >= 1) && (x2 >= 1)){
+                    var v1 = editText1.text.toString().toFloat()
+                    var v2 = editText2.text.toString().toFloat()
+                    var v3 = v1 - v2
+                    intent.putExtra("VALUE1", v3)
+                    startActivity(intent)
+                }
+                else {
+                    textView.text = "数値が入力されていません"
+
+                }
             }
             R.id.button3 -> {
                 val intent = Intent(this, SecondActivity::class.java)
-                val v1 = editText1.text.toString().toFloat()
-                val v2 = editText2.text.toString().toFloat()
-                val v3 = v1 * v2
-                intent.putExtra("VALUE1", v3)
-                startActivity(intent)
+                val x1 = editText1.text.toString().length
+                val x2 = editText2.text.toString().length
+
+                if ((x1 >= 1) && (x2 >= 1)){
+                    var v1 = editText1.text.toString().toFloat()
+                    var v2 = editText2.text.toString().toFloat()
+                    var v3 = v1 * v2
+                    intent.putExtra("VALUE1", v3)
+                    startActivity(intent)
+                }
+                else {
+                    textView.text = "数値が入力されていません"
+
+                }
             }
             R.id.button4 -> {
                 val intent = Intent(this, SecondActivity::class.java)
-                val v1 = editText1.text.toString().toFloat()
-                val v2 = editText2.text.toString().toFloat()
-                val v3 = v1 / v2
-                intent.putExtra("VALUE1", v3)
-                startActivity(intent)
+                val x1 = editText1.text.toString().length
+                val x2 = editText2.text.toString().length
+
+                if ((x1 >= 1) && (x2 >= 1)){
+                    var v1 = editText1.text.toString().toFloat()
+                    var v2 = editText2.text.toString().toFloat()
+                    var v3 = v1 / v2
+                    intent.putExtra("VALUE1", v3)
+                    startActivity(intent)
+                }
+                else {
+                    textView.text = "数値が入力されていません"
+
+                }
             }
+
         }
     }
 }
